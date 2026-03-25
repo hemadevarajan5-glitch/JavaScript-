fe8deac27f9c5d5ff2fcd14b27f3761c73a346a1
Hemalatha D <hemadevarajan5@gmail.com>
2026-03-25 15:41:07 +0530

    Create Grade calculation.js

---
+++ JavaScript Programs/Grade calculation.js
@@ -0,0 +1,11 @@
+let marks = 85;
+
+if (marks >= 90) {
+    console.log("A Grade");
+} else if (marks >= 75) {
+    console.log("B Grade");
+} else if (marks >= 50) {
+    console.log("C Grade");
+} else {
+    console.log("Fail");
+}
